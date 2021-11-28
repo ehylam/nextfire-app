bc885f22c58b9d4957332f9e713c7a593220c6a4
Eric Lam <ehylam.p@gmail.com>
2021-11-28 15:40:54 +1100

    feat: added post/profile page

--- pages/[username]/index.js
+++ pages/[username]/index.js
@@ -1,8 +1,39 @@
-export default function UsernamePage({ params }) {
+import { getUserWithUsername, postToJSON } from '../../lib/firebase';
+import UserProfile from '../../components/UserProfle';
+import PostFeed from '../../components/PostFeed';
+
+export async function getServerSideProps({ query }) {
+  const { username } = query;
+
+  const userDoc = await getUserWithUsername(username); // custom helper function
+
+  let user = null;
+  let posts = null;
+
+  if(userDoc) {
+    user = userDoc.data();
+    const postsQuery = userDoc.ref
+      .collection('posts')
+      .where('published', '==', true)
+      .orderBy('createdAt', 'desc')
+      .limit(5);
+
+      posts = (await postsQuery.get()).docs.map(postToJSON);
+  }
+
+  return {
+    props: { user, posts }
+  }
+}
+
+
+
+export default function UserProfilePage({ user, posts }) {
 
   return (
     <main>
-      <h1>Welcome to my App.</h1>
+      <UserProfile user={user} />
+      <PostFeed posts={posts} />
     </main>
   )
 }
--- pages/enter.js
+++ pages/enter.js
@@ -1,6 +1,7 @@
-import { useContext } from 'react';
+import { useCallback, useContext, useEffect, useState } from 'react';
 import { UserContext } from '../lib/context';
-import { auth, googleAuthProvider } from '../lib/firebase';
+import { auth, googleAuthProvider, firestore } from '../lib/firebase';
+import debounce from 'lodash.debounce';
 
 
 export default function EnterPage() {
@@ -43,12 +44,85 @@ function SignOutButton() {
 }
 
 function UsernameForm() {
+  const [formValue, setFormValue] = useState('');
+  const [isValid, setIsValid] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const { user, username } = useContext(UserContext);
+
+  useEffect(() => {
+    checkUsername(formValue);
+  }, [formValue]);
+
+  const onChange = (e) => {
+    const val = e.target.value.toLowerCase();
+    const re = /^(?=[a-zA-Z0-9._]{3,15}$)(?!.*[_.]{2})[^_.].*[^_.]$/;
+
+    if(val.length < 3) {
+      setFormValue(val);
+      setLoading(false);
+      setIsValid(false);
+    }
+
+    if(re.test(val)) {
+      setFormValue(val);
+      setLoading(true);
+      setIsValid(false);
+    }
+  }
+
+  const checkUsername = useCallback(debounce(async (username) => {
+    if(username.length >= 3) {
+        const ref = firestore.doc(`usernames/${username}`);
+        const { exists } = await ref.get();
+        console.log('Firestore read executed!');
+        setIsValid(!exists);
+        setLoading(false);
+      }
+    },500),
+  []);
+
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    // Create refs for both documents.
+    const userDoc = firestore.doc(`users/${user.uid}`);
+    const usernameDoc = firestore.doc(`usernames/${formValue}`);
+
+    const batch = firestore.batch();
+    batch.set(userDoc, { username: formValue, photoURL: user.photoURL, displayName: user.displayName });
+    batch.set(usernameDoc, { uid: user.uid });
+
+    await batch.commit();
+  }
+
+
+  function UsernameMessage({ username, isValid, loading }) {
+    if(loading) {
+      return <p>Checking username...</p>;
+    } else if(isValid) {
+      return <p className="text-success">Username is available!</p>;
+    } else if(username && !isValid) {
+      return <p className="text-danger">Username is taken!</p>;
+    } else {
+      return <p></p>;
+    }
+  }
+
+
+
   return (
-    <form className="username-form">
-      <label htmlFor="username">Enter your username:</label>
-      <input type="text" id="username" />
-      <button>Submit</button>
-    </form>
+    !username && (
+      <section>
+        <h3>Choose Username</h3>
+        <form onSubmit={onSubmit}>
+          <input name="username" placeholder="Username" value={formValue} onChange={onChange} />
+          <UsernameMessage username={formValue} isValid={isValid} loading={loading} />
+          <button type="submit" className="btn-green" disabled={!isValid}>Choose</button>
+        </form>
+      </section>
+    )
 
   )
 }
